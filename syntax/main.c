b43e016700df4cafa1159aa891a04593b4fced21
최민제 <louis9982.dev@gmail.com>
2023-11-30 15:19:36 +0900

    syntax analysis v0.8

---
+++ syntax/main.c
@@ -0,0 +1,20 @@
+#include "ssu_syntax.h"
+#include "type.h"
+#include "y.tab.h"
+
+A_TYPE *int_type, *char_type, *void_type, *float_type, *string_type;
+A_NODE *root;
+A_ID *current_id=NIL;
+extern FILE *yyin;
+int syntax_err=0;
+int line_no=1;
+int current_level=0;
+int main() {
+	yyin = fopen("test1.c", "r");
+	initialize();
+	yyparse();
+	if (syntax_err)
+		exit(1);
+	print_ast(root); // print syntax tree and tables
+	exit(0);
+}
